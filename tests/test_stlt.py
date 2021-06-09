366ef7607714d65181314373796ae39bd569796f
PureFunctor <purefunctor@gmail.com>
2021-06-09 15:29:07 +0800

    Fix linting errors in test code

--- tests/test_stlt.py
+++ tests/test_stlt.py
@@ -1,5 +1,7 @@
+"""Tests for the top-level `stlt` package."""
 from stlt import __version__
 
 
-def test_stlt():
+def test_stlt() -> None:
+    """Test the `__version__` constant."""
     assert __version__ == "0.1.0"

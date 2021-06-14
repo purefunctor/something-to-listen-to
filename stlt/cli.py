98056dff06bcc4551bb9a6036a25a792dda35a15
PureFunctor <purefunctor@gmail.com>
2021-06-14 22:18:03 +0800

    Create custom click context class

--- stlt/cli.py
+++ stlt/cli.py
@@ -9,12 +9,20 @@ from spotipy import Spotify  # type: ignore
 from spotipy.cache_handler import CacheFileHandler  # type: ignore
 from spotipy.oauth2 import SpotifyOAuth  # type: ignore
 
-from stlt.config import load_config
+from stlt.config import Config, load_config
 from stlt.constants import CONFIG_FILE_PATH
 from stlt.view import create_album_view, create_track_view
 
 
-pass_spotify = click.make_pass_decorator(Spotify)
+@attr.s(frozen=True, slots=True)
+class Context:
+    """Global context used in the command-line interface."""
+
+    client: Spotify = attr.ib()
+    config: Config = attr.ib()
+
+
+pass_context = click.make_pass_decorator(Context)
 
 
 @click.group(
@@ -28,48 +36,51 @@ pass_spotify = click.make_pass_decorator(Spotify)
 )
 @click.pass_context
 def stlt(context: click.Context, config_file: Path) -> None:  # noqa: D103
-    context.ensure_object(dict)
     config = load_config(config_file)
-    context.obj = Spotify(
+    client = Spotify(
         auth_manager=SpotifyOAuth(
             **attr.asdict(config.oauth),
             cache_handler=CacheFileHandler(config.cache.auth_cache),
         )
     )
+    context.obj = Context(
+        client,
+        config,
+    )
 
 
 @stlt.group(name="saved")
-@pass_spotify
-def saved(client: Spotify) -> None:
+@pass_context
+def saved(context: Context) -> None:
     """Query the user's saved items."""
 
 
 @saved.command(name="albums")
 @click.option("-l", "--limit", default=20)
 @click.option("-c", "--columns", default=3)
-@pass_spotify
-def saved_albums(client: Spotify, limit: int, columns: int) -> None:
+@pass_context
+def saved_albums(context: Context, limit: int, columns: int) -> None:
     """List the user's saved albums."""
-    response = client.current_user_saved_albums(limit=limit)
+    response = context.client.current_user_saved_albums(limit=limit)
     print(create_album_view(response["items"], columns=columns))
 
 
 @saved.command(name="tracks")
 @click.option("-l", "--limit", default=20)
 @click.option("-c", "--columns", default=3)
-@pass_spotify
-def saved_tracks(client: Spotify, limit: int, columns: int) -> None:
+@pass_context
+def saved_tracks(context: Context, limit: int, columns: int) -> None:
     """List the user's saved tracks."""
-    response = client.current_user_saved_tracks(limit=limit)
+    response = context.client.current_user_saved_tracks(limit=limit)
     print(create_track_view(response["items"], columns=columns))
 
 
 @stlt.command(name="login")
-@pass_spotify
-def login(client: Spotify) -> None:
+@pass_context
+def login(context: Context) -> None:
     """Log in using Spotify OAuth."""
-    if client.auth_manager.cache_handler.get_cached_token() is not None:
+    if context.client.auth_manager.cache_handler.get_cached_token() is not None:
         click.echo("Logged in!")
     else:
         click.echo("Logging you in...")
-        client.auth_manager.get_access_token()
+        context.client.auth_manager.get_access_token()
